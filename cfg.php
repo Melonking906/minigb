73a4ec9dc198b7385618fc1c4b473bb1db53f9c4
ThatRoboticFish <113400974+ThatRoboticFish@users.noreply.github.com>
2022-12-26 08:55:11 -0300

    Create cfg.php

---
+++ cfg.php
@@ -0,0 +1,14 @@
+<?php
+
+/* minigb included variables file */
+
+// Default username (replace this to your username)
+$default_user = "";
+
+// Max length for posts
+$max_length_name = 40;
+$max_length_email = 250;
+$max_length_url = 250;
+$max_length_comment = 520;
+
+?>
