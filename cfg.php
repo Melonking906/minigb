c45d7b917573691901de64ab9a086e88b09b8d76
ThatRoboticFish <113400974+ThatRoboticFish@users.noreply.github.com>
2022-12-26 08:57:50 -0300

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -5,6 +5,12 @@
 // Default username (replace this to your username)
 $default_user = "";
 
+// Default entries TXT database file
+$database = "entries_$default_user";
+
+// Default CSS style file
+$style = "style_$default_user";
+
 // Max length for posts
 $max_length_name = 40;
 $max_length_email = 250;
