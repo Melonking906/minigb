300428c97a3ae084ef7677e6c03b3c68f54b1f8c
ThatRoboticFish <113400974+ThatRoboticFish@users.noreply.github.com>
2023-06-22 14:15:19 +0000

    Create swatch.php

---
+++ lib/swatch.php
@@ -0,0 +1,24 @@
+<?php
+# https://wiki.melonland.net/swatch_time
+# Returns the current Swatch time with microbeats
+
+function GetSwatchTime($showDecimals = true, $dateTime) {
+	// Get time in UTC+1 (Do not Change!)
+	$now = new DateTime($dateTime, new DateTimeZone("UTC"));
+	$now->add(new DateInterval("PT1H"));
+
+	// Calculate the seconds since midnight e.g. time of day in seconds
+	$midnight = clone $now;
+	$midnight->setTime(0, 0);
+	$seconds = $now->getTimestamp() - $midnight->getTimestamp();
+
+	// Swatch beats in seconds - DO NOT CHANGE
+	$swatchBeatInSeconds = 86.4;
+
+	// Calculate beats to two decimal places
+	if ($showDecimals) {
+		return number_format(round(abs($seconds / $swatchBeatInSeconds), 2), 2);
+	} else {
+        	return floor(abs($seconds / $swatchBeatInSeconds));
+	}
+}
