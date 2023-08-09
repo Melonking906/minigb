e7b8f70f13360e04fdf07a464ed453556857a3e9
ThatRoboticFish <113400974+ThatRoboticFish@users.noreply.github.com>
2023-08-09 13:58:48 +0000

    Update funcs.php

--- include/funcs.php
+++ include/funcs.php
@@ -3,7 +3,6 @@ require_once(__DIR__ . "/../cfg.php");
 
 # https://stackoverflow.com/questions/409496/prevent-direct-access-to-a-php-include-file
 # Redirects a user who tries to access include files from the browser to index.php
-
 if (!defined('IncludeAccess')) {
 	header("Location: ../" . $script_fn);
 	exit;
@@ -11,7 +10,6 @@ if (!defined('IncludeAccess')) {
 
 # https://wiki.melonland.net/swatch_time
 # Returns the current Swatch time with microbeats
-
 function GetSwatchTime($showDecimals = true, $dateTime) {
 	// Get time in UTC+1 (Do not Change!)
 	$now = new DateTime($dateTime, new DateTimeZone("UTC"));
